5ae613f09b60919194515690050f0ceb87caecc5
sthlmj <joehuldens@gmail.com>
2018-11-18 19:28:12 +0100

    Before passing down information between components in the app.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -5,18 +5,7 @@ The purpose of the <Business /> component is to represent how a business (a rest
 import React from 'react';
 import './Business.css';
 
-//make js object
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
+
 
 //make react component class
 class Business extends React.Component {
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,17 +1,18 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
 /*
 The point of the <BusinessList /> component is to simulate what a returned list of businesses would look like in Ravenous (after querying the Yelp API, for example)
  */
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './BusinessList.css';
-import Business from '../Business/Business';
 
 //make react component
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
+                <Business />
+                <Business />
                 <Business />
                 <Business />
                 <Business />
